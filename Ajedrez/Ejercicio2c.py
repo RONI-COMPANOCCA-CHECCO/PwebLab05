abf132e22d83af619cea4922d6c43414a81cf3e3
rcompanocca <ronicompanocca@gmail.com>
2024-05-23 10:28:16 -0500

    Haciendo el ejercicio 2d, tratando de cambiar coigo en picture.py para que se nos pueda ejercutar de la mejor forma

--- Ajedrez/Ejercicio2c.py
+++ Ajedrez/Ejercicio2c.py
@@ -1,11 +1,10 @@
 from interpreter import draw
 from chessPictures import queen
 
-# Crear la imagen de la reina
 queen_white = queen
 
-# Unir la reina consigo misma repetidamente para formar una línea de 4 reinas
+# Unimos la reina consigo misma repetidamente para formar una línea de 4 reinas
 row = queen_white.join(queen_white).join(queen_white).join(queen_white)
 
-# Dibujar la imagen final
+# imagen final
 draw(row)
